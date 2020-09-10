b1f9d61bb397c5dba406f26d1df058452149178a
Khoa Do <happyboy0412@gmail.com>
2020-09-10 20:41:15 +0700

    refine the setup and configuration

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mkdir -pv \
-    ./nginx/cert \
-    ./nginx/config \
-    ./nginx/www
+mkdir -pv ./nginx/conf.d
 
 cp ./template/prod.conf ./nginx/config/default.conf
 cp ./template/docker-compose.example.yml ./docker-compose.yml
