412480dc4f9944e459bd11349152a316acce83ad
Khoa Do <happyboy0412@gmail.com>
2020-09-20 18:35:05 +0700

    correct setup

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 mkdir -pv ./nginx/conf.d
 
-cp ./template/prod.conf ./nginx/config/default.conf
+cp ./template/prod.conf ./nginx/conf.d/default.conf
 cp ./template/docker-compose.prod.yml ./docker-compose.yml
